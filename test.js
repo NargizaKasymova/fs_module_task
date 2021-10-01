462a6348df9570349f040fa1d0b399d4fe5656c9
Nargiza <n.kasymova2021@gmail.com>
2021-10-01 10:24:29 +0600

    writeNewFile for testing fs

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const fs = require('fs')
 
 const readInputs = fs.readFileSync('inputs.txt', 'utf8')
 const readPatterns = fs.readFileSync('patterns.txt', 'utf8')
+
+const writeNewFile = fs.writeFileSync('writeNewFile.txt', `${readInputs}`)
+const completeNewFile = fs.appendFileSync('writeNewFile.txt', `${readPatterns}`)
+
 console.log('_________')
-console.log('Inputs: ', readInputs)
-console.log('Patterns: ', readPatterns)
+console.log(readInputs)
+console.log(readPatterns)
